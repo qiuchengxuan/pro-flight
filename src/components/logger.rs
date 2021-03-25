ce8cb07be65dcc3b832be601562a8460f8552f19
qiuchengxuan <qiuchengxuan@gmail.com>
2021-03-25 20:00:05 +0800

    Implement thread-safe logger
    **How to reproduce**
    
    N/A
    
    **Reason and solution**
    
    log could be used in interrupt context, thus log write
    should be thread-safe

--- src/components/logger.rs
+++ src/components/logger.rs
@@ -1,8 +1,7 @@
-use core::cmp::Ordering;
 use core::fmt::{self, Display, Formatter, Write};
 use core::str::from_utf8_unchecked;
+use core::sync::atomic::{AtomicUsize, Ordering};
 
-use crate::hal::io;
 use crate::sys::jiffies;
 
 #[derive(Copy, Clone, PartialEq)]
@@ -25,7 +24,7 @@ impl Display for Level {
 }
 
 impl PartialOrd for Level {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
         (*self as usize).partial_cmp(&(*other as usize))
     }
 }
@@ -39,9 +38,11 @@ const LEVEL: Level = Level::Warning;
 #[cfg(feature = "log-level-error")]
 const LEVEL: Level = Level::Error;
 
+#[derive(Default)]
 pub struct Logger {
     buffer: &'static mut [u8],
-    index: usize,
+    index: AtomicUsize,
+    writer_count: AtomicUsize,
 }
 
 impl Write for Logger {
@@ -50,50 +51,57 @@ impl Write for Logger {
         if size == 0 {
             return Ok(());
         }
+        self.writer_count.fetch_add(1, Ordering::Relaxed);
+        let index = self.index.fetch_add(1, Ordering::Relaxed);
         let mut buf = [0u8; 2];
         let bytes = c.encode_utf8(&mut buf).as_bytes();
-        self.buffer[self.index % size] = bytes[0];
+        self.buffer[index % size] = bytes[0];
         if bytes.len() > 1 {
-            self.buffer[(self.index + 1) % size] = bytes[1];
+            self.buffer[(index + 1) % size] = bytes[1];
         }
-        self.index += 1;
+        self.writer_count.fetch_sub(1, Ordering::Release);
         Ok(())
     }
 
     fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.writer_count.fetch_add(1, Ordering::Relaxed);
         let mut bytes = s.as_bytes();
         if self.buffer.len() <= bytes.len() {
             bytes = &bytes[..self.buffer.len()];
         }
         let size = self.buffer.len();
-        let index = self.index % size;
+        let index = self.index.fetch_add(bytes.len(), Ordering::Relaxed) % size;
 
         if size - index > bytes.len() {
             self.buffer[index..index + bytes.len()].copy_from_slice(bytes);
-            self.index += bytes.len();
+            self.writer_count.fetch_sub(1, Ordering::Release);
             return Ok(());
         }
 
         let partial_size = size - index;
         self.buffer[index..size].copy_from_slice(&bytes[..partial_size]);
         self.buffer[..bytes.len() - partial_size].copy_from_slice(&bytes[partial_size..]);
-        self.index += bytes.len();
+        self.writer_count.fetch_sub(1, Ordering::Release);
         Ok(())
     }
 }
 
 impl Display for Logger {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        if self.index <= self.buffer.len() {
-            return write!(f, "{}", unsafe { from_utf8_unchecked(&self.buffer[..self.index]) });
+        while self.writer_count.load(Ordering::Relaxed) > 0 {}
+        core::sync::atomic::fence(Ordering::Acquire);
+        let index = self.index.load(Ordering::Relaxed);
+        if index <= self.buffer.len() {
+            return write!(f, "{}", unsafe { from_utf8_unchecked(&self.buffer[..index]) });
         }
-        let index = self.index % self.buffer.len();
+        let index = index % self.buffer.len();
         write!(f, "{}", unsafe { from_utf8_unchecked(&self.buffer[index..]) })?;
         write!(f, "{}", unsafe { from_utf8_unchecked(&self.buffer[..index]) })
     }
 }
 
-static mut LOGGER: Logger = Logger { buffer: &mut [], index: 0 };
+static mut LOGGER: Logger =
+    Logger { buffer: &mut [], index: AtomicUsize::new(0), writer_count: AtomicUsize::new(0) };
 
 #[doc(hidden)]
 pub fn __write_log(args: core::fmt::Arguments, level: Level) {
@@ -115,17 +123,6 @@ pub fn __write_log_literal(message: &'static str, level: Level) {
     let jiffies = jiffies::get();
     let seconds = jiffies.as_secs() as u32;
     writeln!(logger, "[{:5}.{:03}] {}", seconds, jiffies.subsec_millis(), message).ok();
-}
-
-impl Logger {
-    pub fn write<E>(&self, writer: &mut impl io::Write<Error = E>) -> Result<usize, E> {
-        let index = self.index % self.buffer.len();
-        if self.index <= self.buffer.len() {
-            return writer.write(&self.buffer[..index]);
-        }
-        writer.write(&self.buffer[index..])?;
-        writer.write(&self.buffer[..index])
-    }
 }
 
 #[doc(hidden)]
@@ -180,5 +177,5 @@ pub fn get() -> &'static Logger {
 }
 
 pub fn init(buffer: &'static mut [u8]) {
-    unsafe { LOGGER = Logger { buffer, index: 0 } }
+    unsafe { LOGGER = Logger { buffer, ..Default::default() } }
 }
